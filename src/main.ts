b04f99477e1e678173f46b9b4dc8b35de99d1b1d
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-02-08 21:00:55 -0400

    habltando ssl

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,15 @@ import * as fs from "fs";
 async function bootstrap() {
 
 
-  /*const httpsOptions = {
+  const httpsOptions = {
     key: fs.readFileSync('/etc/ssl/quickpay.com.bo/private.key'),
     cert: fs.readFileSync('/etc/ssl/quickpay.com.bo/certificate.crt'),
     ca: fs.readFileSync('/etc/ssl/quickpay.com.bo/ca_bundle.crt'), // Si tienes un certificado intermedio
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
-  });*/
-  const app = await NestFactory.create(AppModule);
+  });
+  //const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new ResponseInterceptor()); // para standrizar las respuestas
   app.useGlobalFilters(new HttpExceptionFilter); // para las respuesats de error
