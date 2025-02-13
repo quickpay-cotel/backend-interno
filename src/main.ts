8881d17917bfc395220c346909167e012e1b5a3e
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-02-12 21:44:24 -0400

    dinalizacion de verificaion entorno

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,32 @@ import * as dotenv from "dotenv";
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/interceptors/http-exception.filter';
 dotenv.config(); // Carga las variables de entorno
-
-import * as fs from "fs";
+import * as fs from 'fs';
 
 async function bootstrap() {
-  /*const httpsOptions = {
-    key: fs.readFileSync('/etc/ssl/quickpay.com.bo/private.key'),
-    cert: fs.readFileSync('/etc/ssl/quickpay.com.bo/certificate.crt'),
-    ca: fs.readFileSync('/etc/ssl/quickpay.com.bo/ca_bundle.crt'), // Si tienes un certificado intermedio
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });*/
-  const app = await NestFactory.create(AppModule);
+  let app;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Configuración con SSL en servidor
+    const httpsOptions = {
+      key: fs.readFileSync('/etc/ssl/quickpay.com.bo/private.key'),
+      cert: fs.readFileSync('/etc/ssl/quickpay.com.bo/certificate.crt'),
+      ca: fs.readFileSync('/etc/ssl/quickpay.com.bo/ca_bundle.crt'), // Si tienes un certificado intermedio
+    };
+    app = await NestFactory.create(AppModule, { httpsOptions });
+  } else {
+    // Configuración sin SSL en local
+    app = await NestFactory.create(AppModule);
+  }
+
   app.enableCors();
-  app.useGlobalInterceptors(new ResponseInterceptor()); // para standrizar las respuestas
-  app.useGlobalFilters(new HttpExceptionFilter); // para las respuesats de error
-  await app.listen(process.env.PORT);
+  app.useGlobalInterceptors(new ResponseInterceptor()); // Para estandarizar las respuestas
+  app.useGlobalFilters(new HttpExceptionFilter()); // Para las respuestas de error
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on ${process.env.NODE_ENV === 'production' ? 'HTTPS' : 'HTTP'} at port ${port}`);
 }
+
 bootstrap();
+
