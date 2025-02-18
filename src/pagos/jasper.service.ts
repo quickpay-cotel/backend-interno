4516e04f5ee8898a00f6761332cb0c941bffa4ac
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-02-17 22:59:25 -0400

    ajustes de reprote

--- src/pagos/jasper.service.ts
+++ src/pagos/jasper.service.ts
@@ -12,31 +12,56 @@ export class JasperService {
 
     }
 
-    async generateReport(consultaPagosDto: ConsultaPagosDto, ext:string ): Promise<string> {
+    async generateReport(consultaPagosDto: ConsultaPagosDto, ext: string): Promise<string> {
 
         let data = await this.cotelDeudasRepository.findPagosRealizados(
             consultaPagosDto.nombreCompleto, consultaPagosDto.servicio, consultaPagosDto.idTransaccion,
-             consultaPagosDto.periodo,consultaPagosDto.codigoDeuda, consultaPagosDto.mensajeDeuda,
-              consultaPagosDto.mensajeContrato,consultaPagosDto.tipoDocumento, 
-              consultaPagosDto.numeroDocumento, consultaPagosDto.fechaInicioPago, consultaPagosDto.fechaFinPago
+            consultaPagosDto.periodo, consultaPagosDto.codigoDeuda, consultaPagosDto.mensajeDeuda,
+            consultaPagosDto.mensajeContrato, consultaPagosDto.tipoDocumento,
+            consultaPagosDto.numeroDocumento, consultaPagosDto.fechaInicioPago, consultaPagosDto.fechaFinPago
         );
+
+
+        if (!data.length) return;
+        let filtroString ='';
+        if (this.verificarAlgunCampoConValor(consultaPagosDto)) {
+            let filtros = Object.fromEntries(
+                Object.entries(consultaPagosDto).filter(([key, value]) => value !== null && value !== "")
+            );
+            filtroString = "<div><strong>Filtros Aplicados:</strong>";
+            // Itera sobre el objeto con un for...of usando Object.entries
+            for (const [key, value] of Object.entries(filtros)) {
+                filtroString += `<li>${key} = ${value}</li>`;
+            }
+            filtroString += "</div>";
+        }
+
+        console.log(filtroString);
+        //let parametro = `-PP_filtros=${filtroString}` 
+        let parametro = `-PP_filtros="${filtroString.replace(/"/g, '\\"')}"`;
 
         const jasperStartetPath = path.join(process.cwd(), 'reportes/jasperstarter/bin/jasperstarter');
 
-        // Cambiar al directorio donde se encuentran los archivos .jrxml y la imagen
+        // Guardar el directorio de trabajo original
+        const rutaInicial = process.cwd();
+        
+        // Cambiar al directorio donde están los archivos .jrxml e imágenes
         const workingDirectory = path.join(process.cwd(), 'jasper-templates/pagos_realizados');
-        process.chdir(workingDirectory);
-
+        await process.chdir(workingDirectory);
+        
+        // Definir las rutas de los archivos
         const jsonFilePath = path.join(process.cwd(), 'pagos_realizados.json');
         const jrxmlFilePath = path.join(process.cwd(), 'pagos_realizados.jrxml');
         const outputFilePath = path.join(process.cwd(), 'pagos_realizados');
         
+        // Escribir el archivo JSON
         await fs.writeFileSync(jsonFilePath, JSON.stringify(data));
-
-        return new Promise((resolve, reject) => {
-            const command = `${jasperStartetPath} pr -t json --json-query "." --data-file ${jsonFilePath} -f ${ext} -o ${outputFilePath} ${jrxmlFilePath}`;
-            console.log(command);
         
+        // Ejecutar el comando para generar el reporte
+        return new Promise((resolve, reject) => {
+            const command = `${jasperStartetPath} pr -t json --json-query "." --data-file ${jsonFilePath} -f ${ext} -o ${outputFilePath} ${jrxmlFilePath} ${parametro}`;
+            console.log(command);
+            
             exec(command, (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error generando el reporte: ${error.message}`);
@@ -44,8 +69,22 @@ export class JasperService {
                     return;
                 }
                 console.log(`Reporte generado: ${stdout}`);
+                
+                // Volver al directorio inicial antes de generar el segundo reporte
+                process.chdir(rutaInicial);
                 resolve(outputFilePath + "." + ext);
             });
         });
+        
+    }
+    // Función para verificar si los campos no están vacíos
+    verificarAlgunCampoConValor(obj) {
+        for (const [key, value] of Object.entries(obj)) {
+            // Verifica si el valor NO es null, undefined ni una cadena vacía
+            if (value !== null && value !== undefined && value !== "") {
+                return true;  // Si encontramos un campo lleno, devolvemos true
+            }
+        }
+        return false;  // Si todos los campos están vacíos, devolvemos false
     }
 }
