cc224ae1d8cfe853ce66831b2d102c6affa613ba
alvaroquispesegales <alvaroquispesegales@outlook.com>
2025-07-06 22:30:06 -0400

    no se q se ha cabiado

--- src/pagos/cargas-excel/cargas-excel.controller.ts
+++ src/pagos/cargas-excel/cargas-excel.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, UploadedFile, UseGuards, UseInterceptors,Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+  Request,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CargasExcelService } from './cargas-excel.service';
 import { diskStorage } from 'multer';
@@ -7,28 +14,28 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('cargas-excel')
 export class CargasExcelController {
+  constructor(private readonly cargasExcelService: CargasExcelService) {}
 
-  constructor(private readonly cargasExcelService: CargasExcelService) { }
-
-@Post('upload')
-@UseGuards(JwtAuthGuard) // Protege el endpoint con el guardia JWT
-@UseInterceptors(FileInterceptor('file', {
-  storage: diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.join(process.cwd(), 'store', 'deudas'));
-    },
-    filename: (req, file, cb) => {
-      const filename = `${Date.now()}-${file.originalname}`;
-      cb(null, filename);
-    },
-  }),
-}))
-async uploadExcel(
-  @UploadedFile() file: Express.Multer.File,
-  @Request() req: any
-) {
-  const user = req.user.sub; // Aquí está el usuario del token
-  return await this.cargasExcelService.procesarExcel(file, user);
-}
-
+  @Post('upload')
+  @UseGuards(JwtAuthGuard) // Protege el endpoint con el guardia JWT
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, path.join(process.cwd(), 'store', 'deudas'));
+        },
+        filename: (req, file, cb) => {
+          const filename = `${Date.now()}-${file.originalname}`;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  async uploadExcel(
+    @UploadedFile() file: Express.Multer.File,
+    @Request() req: any,
+  ) {
+    const user = req.user.sub; // Aquí está el usuario del token
+    return await this.cargasExcelService.procesarExcel(file, user);
+  }
 }
